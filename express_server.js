448e07f697f56b324f7b81c9fc4d71e644d90e26
Bernard Yang <ybernard@gmail.com>
2021-06-08 02:13:24 +0000

    added ejs view engine

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,13 +22,7 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-const express = require("express");
-const app = express();
-const PORT = 8080;
-
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.set("view engine", "ejs");
