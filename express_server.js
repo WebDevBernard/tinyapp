024d59290c9691c5cc0e294683f9458b91a26f79
Bernard Yang <ybernard@gmail.com>
2021-06-08 22:03:15 +0000

    spelling correction + added button and routing for delete

--- express_server.js
+++ express_server.js
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 
 //Generates 6 character random string
 const generateRandomString = () => {
-  return Math.random().toString(36).substring(6);
+  return Math.random().toString(36).substring(2, 8);
 };
 
 const urlDatabase = {
@@ -51,8 +51,13 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[res.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
