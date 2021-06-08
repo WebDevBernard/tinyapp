9206584bfa999b3b54917ec195f8b4cf29f8c7df
Bernard Yang <ybernard@gmail.com>
2021-06-08 05:13:56 +0000

    added bodyparser for POST requests

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,13 +11,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 
