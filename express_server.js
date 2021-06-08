56a5209155db70da0bb59e5d36828b126ecd7a3f
Bernard Yang <ybernard@gmail.com>
2021-06-08 05:48:18 +0000

    added a function that creates a 6 char random string

--- express_server.js
+++ express_server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const bodyParser = require("body-parser");
 
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+//Generates 6 character random string
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(6);
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -35,6 +41,11 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
