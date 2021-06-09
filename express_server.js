dafff6ac999329ab16fb141a4658ea19beac960c
Bernard Yang <ybernard@gmail.com>
2021-06-09 22:16:10 +0000

    grouped middleware, remove bodyparser, added req.cookies to all templateVars

--- express_server.js
+++ express_server.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-
-app.set("view engine", "ejs");
-
 //Generates 6 character random string
 const generateRandomString = () => {
   return Math.random().toString(36).substring(2, 8);
 };
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -37,13 +34,18 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL,  username: req.cookies["username"] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
@@ -51,11 +53,6 @@ app.post("/urls", (req, res) => {
   const shortUrl = generateRandomString();
   urlDatabase[shortUrl] = req.body.longURL;
   res.redirect(`/urls/${shortUrl}`);
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
